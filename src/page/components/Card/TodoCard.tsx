8468bdc795a93ecc0bd120051877c9147767610b
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-04-09 11:18:06 +0300

    feat: edit mode
    button in Todo card

--- src/page/components/Card/TodoCard.tsx
+++ src/page/components/Card/TodoCard.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import { TodoCardProps } from './TodoCard.props';
 import styles from './TodoCard.module.css';
-import cn from 'classnames';
-import { TodoStatus } from '../../../enums/enums';
-import { DeleteTodoButton } from '../../../components_common';
+import { DeleteTodoButton, EditTodoButton } from '../../../components_common';
 
 const TodoCard = (props: TodoCardProps): JSX.Element => {
-  const { id, title, description, status, onDelete } = props;
+  const { id, title, description, status, onDelete, onOpenEditModal } = props;
 
-  const cardStyles = cn(styles.card, {
-    [styles.completed]: status === TodoStatus.completed,
-    [styles.inProgress]: status === TodoStatus.inProgress,
-    [styles.planned]: status === TodoStatus.planned,
-  });
+  const handlerOnDelete = () => {
+    onDelete(id);
+  };
+  const handlerOnOpenEditModal = () => {
+    const todo = { id, title, description, status };
+    onOpenEditModal(todo);
+  };
 
   return (
-    <div className={cardStyles}>
-      <DeleteTodoButton
-        className={styles.deleteButton}
-        onClick={() => {
-          onDelete(id);
-        }}
-      />
-      <div>Title: {title}</div>
+    <div className={styles.card}>
+      <DeleteTodoButton className={styles.deleteButton} onClick={handlerOnDelete} />
+      <EditTodoButton className={styles.editButton} onClick={handlerOnOpenEditModal} />
+      <div>
+        Title: <b>{title}</b>
+      </div>
       <div>Description: {description}</div>
     </div>
   );

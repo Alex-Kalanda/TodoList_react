874f9ebd0da82771a2362745c9e4ca622e9d439a
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-04-11 23:01:59 +0300

    Squashed following commits:
    commit 2bb55970022b98672c1c9c0d8a86cdc236ff6458
    commit a4fae7fb21f9dd34ff915c4ba21ce7cdc06dd79d
    commit cd33b760743785a206328caacb4c53d62a92dd7a
    commit aaee89448ac1093c07f1a6f6d47a69a78b0037ad
    commit 73812118a5f99228cf6c1c8a94e0862a328e19c6
    commit 5943fb01041faee84916413c5efe99db8b002cc4
    commit 68ac231c27a8182ef8c919ec3509de469a3c5cf5
    commit e39f6d61a331620dcb89c14443f97177538ebf2c
    commit 33a4d27955e2d6c53d55128e7d4e935b35ebe0e8
    commit bb99d5bbd00234ec41afd1aeb102d777dcaaeeb2
    commit 8468bdc795a93ecc0bd120051877c9147767610b
    commit 5527ba7205b1825a3683d82f24429ae27dc36cd6
    commit a022dde6f5b665cd1e7f2f57481cca5f7e6fc1da
    commit 4c2e91a66cbe356e595571e8ab9216f209c4f015
    commit 2daff1fb7d1573cb5bca886ed6a07b6c96ea0314
    commit 8321fc7030289b6c7e04b564ce3ba9f0b003a51a
    commit 889aea994f7f5c3184d36fe28755b51bd6e98c37
    commit 1e0279b83e420fc25d20d10989146fb70e20a784
    commit 69bfbcbcee8f21498994d76c2e553f6b42be4a9a
    commit ef60fbd2bca5fa5fb01dfde7cf7fb47e03f82d66
    commit 129add79c67fc0bbb03dc07697226eb4e1c19fea
    commit cfc3992a97a70662eee1447739fc7081a2065156

--- src/page/components/index.ts
+++ src/page/components/index.ts
@@ -3,5 +3,7 @@ import TodoCard from './Card/TodoCard';
 import EmptyCard from './EmptyCard/EmptyCard';
 import FormCreateTodo from './form/FormCreateTodo';
 import FormEditTodo from './form/FormEditTodo';
+import FilterBar from './FilterBar/FilterBar';
+import EmptyCardFilter from './EmptyCard/EmptyCardFilter';
 
-export { StatusBox, TodoCard, EmptyCard, FormCreateTodo, FormEditTodo };
+export { StatusBox, TodoCard, EmptyCard, FormCreateTodo, FormEditTodo, FilterBar, EmptyCardFilter };

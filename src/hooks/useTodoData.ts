df77b89cb8464c78e74ef0337c0b63be783ab6af
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-04-14 11:08:25 +0300

    refactor: useEffect >>> async/await

--- src/hooks/useTodoData.ts
+++ src/hooks/useTodoData.ts
@@ -1,24 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { TodoResponse } from '../page/MainPage/components/Card/TodoCard.props';
-import axios, { AxiosResponse } from 'axios';
 
 interface UseTodoData {
-  id?: string;
+  id: string;
 }
+
+const getSingleTodo = async (id: string) => {
+  const response: Response = await fetch(`${process.env.REACT_APP_TODO_ENDPOINT}/${id}`);
+  return response.json();
+};
 
 const useTodoData = ({ id }: UseTodoData): TodoResponse | null => {
   const [todo, setTodo] = useState<TodoResponse | null>(null);
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_TODO_ENDPOINT}/${id}`)
-      .then((response: AxiosResponse) => {
-        if (response.data) {
-          setTodo(response.data);
-        }
-      })
-      .catch((error) => {
-        throw error;
-      });
+
+  getSingleTodo(id).then((response) => {
+    setTodo(response);
   });
 
   return todo;

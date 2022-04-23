0cd68f86ba6386d197b613c6767637bdfd6e3c26
Aliaksandar <gumanist54@gmail.com>
2022-04-23 14:33:42 +0300

    refactor: useGetTodo

--- src/hooks/useGetTodo.ts
+++ src/hooks/useGetTodo.ts
@@ -8,18 +8,16 @@ import { emptyTodo, Todo } from '../page/MainPage/components/Card/TodoCard.props
 const useGetTodo = (todos: Todo[]): Todo => {
   const dispatch = useDispatch();
   const { id = '' } = useParams();
-  const currentId = id;
-  const isEmptyTodos = todos.length === 0;
   const [todo, setTodo] = useState(emptyTodo);
 
   useEffect(() => {
-    if (isEmptyTodos) {
+    if (todos.length === 0) {
       getSingleTodoApi(id).then((r) => {
         setTodo(r);
         dispatch(finishLoading());
       });
     } else {
-      setTodo(todos.find(({ id }) => id === currentId) as Todo);
+      setTodo(todos.find((todo) => todo.id === id) as Todo);
     }
   }, [dispatch, id, todos]);
 

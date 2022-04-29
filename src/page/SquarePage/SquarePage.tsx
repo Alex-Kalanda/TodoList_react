51628a694e83c9bdf8ffed358ce799f05178d8b7
Aliaksandar <gumanist54@gmail.com>
2022-04-29 23:56:02 +0300

    refactor: enhancement with useCallback

--- src/page/SquarePage/SquarePage.tsx
+++ src/page/SquarePage/SquarePage.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './SquarePage.module.css';
 
 const SquarePage = () => {
   const squareRef = useRef(null);
+  const [counter, setCounter] = useState(0);
 
-  const handleChangeSize = () => {
+  const changeSize = () => {
     const square = squareRef.current as HTMLElement | null;
 
     if (square) {
@@ -17,15 +18,37 @@ const SquarePage = () => {
       }
     }
   };
+  const handleChangeSize = useCallback(changeSize, []);
+
+  const handleIncreaseCounter = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const handleReduceCounter = () => {
+    setCounter((prev) => prev - 1);
+  };
 
   useEffect(() => {
-    window.addEventListener('resize', handleChangeSize);
+    const square = squareRef.current as HTMLElement | null;
     handleChangeSize();
-  }, []);
+
+    if (square && counter > 5) {
+      window.addEventListener('resize', handleChangeSize);
+    } else {
+      window.removeEventListener('resize', handleChangeSize);
+    }
+    return () => {
+      window.removeEventListener('resize', handleChangeSize);
+    };
+  }, [counter, handleChangeSize]);
 
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.caption}>Mysterious square</h1>
+      <div>
+        <button onClick={handleReduceCounter}>Reduce</button>
+        <button onClick={handleIncreaseCounter}>Increase</button>
+      </div>
+      <span>Counter value = {counter}</span>
       <div ref={squareRef} className={styles.square} />
     </div>
   );
